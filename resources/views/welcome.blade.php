259f8246b0f6eec3542d6ea1d896fc5351e58e76
Danish Ali Furniturewala <danishali7866@gmail.com>
2017-09-25 12:10:42 +0530

    Fixed responsiveness on welcome page search results

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
         <h1 class="pacifico">Welcome to Chirp.io</h1>
         <div class="row no-margin">
             <form action="">
-                <div class="form-group col-lg-6 col-lg-push-3 col-md-4 col-md-push-4 col-sm-6 col-sm-push-3 col-xs-12 margin-top-10">
+                <div class="form-group col-lg-6 col-lg-push-3 col-md-8 col-md-push-2 col-sm-10 col-sm-push-1 col-xs-12 margin-top-10">
                     <input id="main-page-search-field" placeholder="Search what's happening around" type="text" name="search" id="search">
                     <div class="text-left col-lg-10 col-lg-push-1 col-md-10 col-md-push-1 col-sm-10 col-sm-push-1 col-xs-10 col-xs-push-1" id="main-results-dropdown" style="display:none">
                         <ul class="list-unstyled" id="search-results">
