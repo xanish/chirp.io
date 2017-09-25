1fbf1af8601152f63cf86205bfab77fe97ecffda
Danish Ali Furniturewala <danishali7866@gmail.com>
2017-09-25 18:52:12 +0530

    gif support in tweet, minor fixes.

--- app/Utils/Utils.php
+++ app/Utils/Utils.php
@@ -9,18 +9,25 @@ class Utils
     public function fitAndSaveImage($user_id, $image, $width, $height, $location, $operation)
     {
         $image_name = $user_id.'_'.time().'.'.$image->getClientOriginalExtension();
-        if ($operation == 'fit') {
-            Image::make($image)->fit($width, $height, function ($constraint) {
-                $constraint->upsize();
-            })->save($location.'/'.$image_name);
-            Image::make($image)->save($location.'/original_'.$image_name);
+        if ($image->getClientOriginalExtension() == 'gif') {
+            $file = Image::make($image->getRealPath());
+            copy($image->getRealPath(), $location.'/original_'.$image_name);
+            copy($image->getRealPath(), $location.'/'.$image_name);
         }
         else {
-            Image::make($image)->resize($width, $height, function ($constraint) {
-                $constraint->aspectRatio();
-                $constraint->upsize();
-            })->save($location.'/'.$image_name);
-            Image::make($image)->save($location.'/original_'.$image_name);
+            if ($operation == 'fit') {
+                Image::make($image)->fit($width, $height, function ($constraint) {
+                    $constraint->upsize();
+                })->save($location.'/'.$image_name);
+                Image::make($image)->save($location.'/original_'.$image_name);
+            }
+            else {
+                Image::make($image)->resize($width, $height, function ($constraint) {
+                    $constraint->aspectRatio();
+                    $constraint->upsize();
+                })->save($location.'/'.$image_name);
+                Image::make($image)->save($location.'/original_'.$image_name);
+            }
         }
         return $image_name;
     }
