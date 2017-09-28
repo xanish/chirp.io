dc243c58f341991bb120e7ed2d62c5a0dac8272c
Danish Ali Furniturewala <danishali7866@gmail.com>
2017-09-28 17:17:28 +0530

    added master.min.js

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -30,17 +30,18 @@
     <!-- Scripts -->
     <!-- <script src="https://code.jquery.com/jquery-2.2.4.min.js"
     integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script> -->
-    <script src="{{ asset('js/app.js') }}" type="text/javascript"></script>
-    <script src="{{ asset('js/mdb.min.js') }}" type="text/javascript"></script>
     <script>
     var color = {!! json_encode($color) !!}
     </script>
+    <script src="{{ asset('js/master.min.js') }}" type="text/javascript"></script>
+    <!-- <script src="{{ asset('js/app.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('js/mdb.min.js') }}" type="text/javascript"></script>
     <script src="{{ asset('js/custom.js') }}" type="text/javascript"></script>
     <script src="{{ asset('js/emojionearea.js') }}"></script>
     <script src="{{ asset('js/disablescroll.js') }}"></script>
     <script src="{{ asset('js/emoji.js') }}"></script>
     <script src="{{ asset('js/lightbox.js') }}" type="text/javascript"></script>
-    <script src="{{ asset('js/bootstrap-datepicker.min.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('js/bootstrap-datepicker.min.js') }}" type="text/javascript"></script> -->
     <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCso5QRQXnxgph82Q8tV3oYj24SG56jnCc&libraries=places"></script>
 </body>
 </html>
--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -7,12 +7,15 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('css/master.min.css') }}" rel="stylesheet">
+    <!-- <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"> -->
+    <!-- <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('css/mdb.min.css') }}" rel="stylesheet">
     <link href="{{ asset('css/custom.css') }}" rel="stylesheet">
     <link href="{{ asset('css/emojionearea.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/emoji.css') }}" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link href="{{ asset('css/emoji.css') }}" rel="stylesheet"> -->
+    <!-- <link rel="stylesheet" href="{{ asset('css/lightbox.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/bootstrap-datepicker3.min.css') }}"> -->
 </head>
 <body>
     <div id="app">
@@ -25,13 +28,15 @@
     <!-- <script src="https://code.jquery.com/jquery-2.2.4.min.js"
     integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script> -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/zxcvbn/4.2.0/zxcvbn.js" async></script>
-    <script src="{{ asset('js/app.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('js/master.min.js') }}" type="text/javascript"></script>
+    <!-- <script src="{{ asset('js/app.js') }}" type="text/javascript"></script>
     <script src="{{ asset('js/mdb.min.js') }}" type="text/javascript"></script>
     <script src="{{ asset('js/custom.js') }}" type="text/javascript"></script>
     <script src="{{ asset('js/emojionearea.js') }}"></script>
     <script src="{{ asset('js/disablescroll.js') }}"></script>
     <script src="{{ asset('js/emoji.js') }}"></script>
     <script src="{{ asset('js/lightbox.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('js/bootstrap-datepicker.min.js') }}" type="text/javascript"></script> -->
     <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCso5QRQXnxgph82Q8tV3oYj24SG56jnCc&libraries=places"></script>
 </body>
 </html>
